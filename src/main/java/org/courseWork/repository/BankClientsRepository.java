b670de5698a15ec1c6db8fd03982046f4adeab9d
bannikov_ilya <101068317+b4ntik@users.noreply.github.com>
2025-10-30 22:35:49 +0500

    initial commit

---
+++ src/main/java/org/courseWork/repository/BankClientsRepository.java
@@ -0,0 +1,8 @@
+package org.courseWork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankClientsRepository extends JpaRepository {
+}
